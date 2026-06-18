40a452aab6b706f04fe2e3433376888848c58fcb
Punam Sardar <punamsardar791@gmail.com>
2026-06-18 20:42:48 +0530

    Added Contact Us section

--- src/App.jsx
+++ src/App.jsx
@@ -57,14 +57,14 @@ function App() {
       {/* Custom cursor Ring */}
       <div
         ref={outlineRef}
-        className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]"
+        className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none [z-9999]"
         style={{ transition: "transform-0.1s ease-out" }}
       ></div>
 
       {/* cutom cursor dot */}
       <div
         ref={dotRef}
-        className=" fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]"
+        className=" fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none [z-9999]"
       ></div>
     </div>
   );
